f75146ed0d7502bc0af7db65306f2b564a5f6817
Ivan Psarew <psarew.ivan@yandex.ru>
2025-08-18 03:36:38 +0600

    fix: group time

--- app/features/Messages/Messages.tsx
+++ app/features/Messages/Messages.tsx
@@ -1,8 +1,9 @@
-import { Fragment, useRef, useState } from 'react';
+import { useMemo, useRef } from 'react';
 import { observer } from 'mobx-react-lite';
 import classNames from 'classnames';
 import { useStore } from '~/shared/providers/StoreContext';
 import { getFormattedTime } from '~/shared/helpers/getFormattedTime';
+import type { MessageType } from '~/shared/types/message';
 import Message from '~/shared/ui/Message/Message';
 import ChatContainer from '~/shared/ui/ChatContainer/ChatContainer';
 import styles from './Messages.module.scss';
@@ -14,51 +15,61 @@ type Props = {
 function Messages({ className }: Props) {
     const { activeUserId, messagesForActiveUser } = useStore();
 
-    const [currentDate, setCurrentDate] = useState<string>('');
-
     const hasPrevOwnRef = useRef(false);
-    const prevDateRef = useRef<string | null>(null);
+
+    const groupedMessages = useMemo(() => {
+        const result: MessageType[][] = [];
+        let group: MessageType[] = [];
+        let prevDate: string | null = null;
+
+        messagesForActiveUser.forEach((message) => {
+            const msgDate = new Date(message.timestamp);
+            const msgDateKey = msgDate.toISOString().split('T')[0];
+            prevDate = prevDate === null ? msgDateKey : prevDate;
+            const hasGroup = prevDate === msgDateKey;
+
+            if (hasGroup) {
+                group.push(message);
+            } else {
+                prevDate = msgDateKey;
+                result.push(group);
+                group = [];
+            }
+        }, []);
+
+        return result;
+    }, [messagesForActiveUser]);
 
     return (
         <ChatContainer className={className} messages={messagesForActiveUser}>
-            {messagesForActiveUser.map((message, index) => {
-                const hasOwn = message.receiverId === activeUserId;
+            {groupedMessages.map((group, index) => (
+                <section key={index}>
+                    <div className={styles['date']}>
+                        {getFormattedTime(group[0].timestamp, true)}
+                    </div>
+                    {group.map((message, index) => {
+                        const hasOwn = message.receiverId === activeUserId;
+                        const classNameMargin =
+                            index !== 0 && hasOwn === hasPrevOwnRef.current
+                                ? styles['message_self']
+                                : styles['message_group'];
 
-                const classNameMargin =
-                    index !== 0 && hasOwn === hasPrevOwnRef.current
-                        ? styles['message_self']
-                        : styles['message_group'];
-
-                hasPrevOwnRef.current = hasOwn;
-
-                const msgDate = new Date(message.timestamp);
-                const msgDateKey = msgDate.toISOString().split('T')[0];
-                const showDateDivider = prevDateRef.current !== msgDateKey;
-
-                if (showDateDivider) {
-                    prevDateRef.current = msgDateKey;
-                }
-
-                return (
-                    <Fragment key={message.id}>
-                        {showDateDivider && (
-                            <div className={styles['date']}>
-                                {getFormattedTime(msgDate, true)}
-                            </div>
-                        )}
-                        <Message
-                            className={classNames(classNameMargin, {
-                                [styles['message_active']]: hasOwn,
-                            })}
-                            attachments={message.attachments}
-                            hasOwn={message.receiverId === activeUserId}
-                            status={message.status}
-                            text={message.text}
-                            timestamp={message.timestamp}
-                        />
-                    </Fragment>
-                );
-            })}
+                        return (
+                            <Message
+                                key={message.id}
+                                className={classNames(classNameMargin, {
+                                    [styles['message_active']]: hasOwn,
+                                })}
+                                attachments={message.attachments}
+                                hasOwn={message.receiverId === activeUserId}
+                                status={message.status}
+                                text={message.text}
+                                timestamp={message.timestamp}
+                            />
+                        );
+                    })}
+                </section>
+            ))}
         </ChatContainer>
     );
 }
